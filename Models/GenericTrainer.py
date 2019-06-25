76ace3bb133e220ba576b5b62150440adfe0019c
André LS Meirelles <alsmeirelles@gmail.com>
2019-06-25 17:47:32 -0300

    Remodeling of the GenericTrainer to use only one environment setting

--- Models/GenericTrainer.py
+++ Models/GenericTrainer.py
@@ -84,159 +84,15 @@ class Trainer(object):
 
         self._rex = r'{0}-t(?P<try>[0-9]+)e(?P<epoch>[0-9]+).h5'.format(net_model.name)
         
-        if self._config.delay_load:
-            return self.train_model_iterator(net_model)
-        else:
-            return self.train_model(net_model)
-    
-    def train_model(self,model):
+        return self.train_model(net_model)
+
+    def _choose_generator(self,train_data,val_data):
         """
-        Execute training according to configurations. Load all data at once.
-
-        @param model <Keras trainable model>: model to be trained
+        Returns a tuple with two batch generators: (train_generator,val_generator)
+        The type of generator depends on the config.delay_load option
         """
-        rcomp = re.compile(self._rex)
+        train_generator,val_generator = (None,None)
         
-        train,val,_ = self._ds.load_data(split=self._config.split,keepImg=False)
-                    
-        x_train,y_train = train
-        x_val,y_val = val
-
-        #Labels should be converted to categorical representation
-        y_train = to_categorical(y_train,self._ds.nclasses)
-        y_val = to_categorical(y_val,self._ds.nclasses)
-        
-        # session setup
-        sess = K.get_session()
-        ses_config = tf.ConfigProto(
-            device_count={"CPU":self._config.cpu_count,"GPU":self._config.gpu_count},
-            intra_op_parallelism_threads=self._config.cpu_count if self._config.gpu_count == 0 else self._config.gpu_count, 
-            inter_op_parallelism_threads=self._config.cpu_count if self._config.gpu_count == 0 else self._config.gpu_count,
-            log_device_placement=True if self._verbose > 1 else False
-            )
-        sess.config = ses_config
-        K.set_session(sess)
-
-        train_generator = ImageDataGenerator(
-            samplewise_center=self._config.batch_norm,
-            samplewise_std_normalization=self._config.batch_norm,
-            rotation_range=180,
-            width_shift_range=20,
-            height_shift_range=20,
-            zoom_range=.2,
-            #shear_range=.05,
-            horizontal_flip=True,
-            vertical_flip=True,
-            brightness_range=(-20.0,20.0))
-
-        val_generator = ImageDataGenerator(
-            samplewise_center=self._config.batch_norm,
-            samplewise_std_normalization=self._config.batch_norm)
-
-
-        single,parallel = model.build()
-        if not parallel is None:
-            training_model = parallel
-        else:
-            training_model = single
-        
-        # try to resume the training
-        weights = list(filter(lambda f: f.endswith(".h5") and f.startswith(model.name),os.listdir(self._config.weights_path)))
-        weights.sort()
-        old_e_offset = 0
-        if len(weights) > 0 and not self._config.new_net:
-            # get last file (which is the furthest on the training) if exists
-            ep_weights_file = weights[len(weights)-2]
-            match = rcomp.fullmatch(ep_weights_file)
-            if match:
-                old_e_offset = int(match.group('epoch'))
-            else:
-                old_e_offset = 0
-
-            # load weights
-            try:
-                single.load_weights(os.path.join(self._config.weights_path,
-                    ep_weights_file))
-                if self._verbose > 0:
-                    print("Sucessfully loaded previous weights: {0}".format(ep_weights_file))
-            except ValueError:
-                single.load_weights(os.path.join(self._config.weights_path,"{0}-weights.h5".format(model.name)))
-                if self._verbose > 0:
-                    print("Sucessfully loaded previous weights from consolidated file.")
-            except ValueError:
-                print("[ALERT] Could not load previous weights, training from scratch")
-                
-        wf_header = "{0}-t{1}".format(model.name,old_e_offset+1)
-
-        ### Define special behaviour CALLBACKS
-        callbacks = []
-        ## ModelCheckpoint
-        callbacks.append(ModelCheckpoint(os.path.join(
-            self._config.weights_path, wf_header + "e{epoch:02d}.h5"), 
-            save_weights_only=True, period=1,save_best_only=True,monitor='val_acc'))
-        ## ReduceLROnPlateau
-        callbacks.append(ReduceLROnPlateau(monitor='val_loss',factor=0.4,\
-                                           patience=3,verbose=self._verbose,\
-                                           mode='auto',min_lr=1e-6))        
-
-        if self._config.info:
-            print(training_model.summary())
-
-        training_model.fit_generator(
-            generator = train_generator.flow(x_train,y_train,batch_size=self._config.batch_size,shuffle=True),
-            steps_per_epoch = len(x_train) // self._config.batch_size,
-            epochs = self._config.epochs,
-            validation_data = val_generator.flow(x_val,y_val,batch_size=1),
-            validation_steps = len(x_val) //self._config.batch_size,
-            verbose = 1 if self._verbose > 0 else 0,
-            use_multiprocessing = False,
-            workers=3,
-            max_queue_size=45,
-            callbacks=callbacks)
-
-        #Weights should be saved only through the plain model
-        cache_m = CacheManager()
-        single.save_weights(model.get_weights_cache())
-        single.save(model.get_model_cache())
-        cache_m.dump(self._config.split,'split_ratio.pik')
-
-        return Exitcodes.ALL_GOOD
-    
-    def train_model_iterator(self,model):
-        """
-        Use the fit_iterator to control the sample production
-        TODO: for future use
-        """
-        from Models import ThreadedGenerator
-
-        rcomp = re.compile(self._rex)
-        
-        # session setup
-        sess = K.get_session()
-        ses_config = tf.ConfigProto(
-            device_count={"CPU":self._config.cpu_count,"GPU":self._config.gpu_count},
-            intra_op_parallelism_threads=self._config.cpu_count if self._config.gpu_count == 0 else self._config.gpu_count, 
-            inter_op_parallelism_threads=self._config.cpu_count if self._config.gpu_count == 0 else self._config.gpu_count,
-            log_device_placement=True if self._verbose > 1 else False
-            )
-        sess.config = ses_config
-        K.set_session(sess)
-
-        #Setup of generators, augmentation, preprocessing
-        train_data,val_data,_ = self._ds.split_metadata(self._config.split)
-        
-        if self._verbose > 0:
-            unique,count = np.unique(train_data[1],return_counts=True)
-            l_count = dict(zip(unique,count))
-            print("Train labels: {0} are 0; {1} are 1;\n - {2:.2f} are positives".format(l_count[0],l_count[1],(l_count[1]/(l_count[0]+l_count[1]))))
-            
-            unique,count = np.unique(val_data[1],return_counts=True)
-            l_count = dict(zip(unique,count))
-            print("Validation labels: {0} are 0; {1} are 1;\n - {2:.2f} are positives".format(l_count[0],l_count[1],(l_count[1]/(l_count[0]+l_count[1]))))
-            
-            print("Train set: {0} items".format(len(train_data[0])))
-            print("Validate set: {0} items".format(len(val_data[0])))
-            
         train_prep = ImageDataGenerator(
             samplewise_center=self._config.batch_norm,
             samplewise_std_normalization=self._config.batch_norm,
@@ -249,28 +105,79 @@ class Trainer(object):
             vertical_flip=True,
             brightness_range=(-20.0,20.0))
 
+        val_prep = ImageDataGenerator(
+            samplewise_center=self._config.batch_norm,
+            samplewise_std_normalization=self._config.batch_norm)
+        
         if not self._config.tdim is None:
             fix_dim = self._config.tdim
         else:
             fix_dim = self._ds.get_dataset_dimensions()[0][1:] #Only smallest image dimensions matter here
-        train_generator = ThreadedGenerator(dps=train_data,
-                                            classes=self._ds.nclasses,
-                                            dim=fix_dim,
-                                            batch_size=self._config.batch_size,
-                                            image_generator=train_prep,
-                                            shuffle=True,
-                                            verbose=self._config.verbose)
 
-        val_prep = ImageDataGenerator(
-            samplewise_center=self._config.batch_norm,
-            samplewise_std_normalization=self._config.batch_norm)
-        val_generator = ThreadedGenerator(dps=val_data,
-                                            classes=self._ds.nclasses,
-                                            dim=fix_dim,
-                                            batch_size=self._config.batch_size,
-                                            image_generator=val_prep,
-                                            shuffle=True,
-                                            verbose=self._config.verbose)
+        if self._config.delay_load:
+            from Models import ThreadedGenerator
+            
+            train_generator = ThreadedGenerator(dps=train_data,
+                                                classes=self._ds.nclasses,
+                                                dim=fix_dim,
+                                                batch_size=self._config.batch_size,
+                                                image_generator=train_prep,
+                                                shuffle=True,
+                                                verbose=self._config.verbose)
+            
+            val_generator = ThreadedGenerator(dps=val_data,
+                                                classes=self._ds.nclasses,
+                                                dim=fix_dim,
+                                                batch_size=self._config.batch_size,
+                                                image_generator=val_prep,
+                                                shuffle=True,
+                                                verbose=self._config.verbose)
+        else:
+            train,val,_ = self._ds.load_data(split=self._config.split,keepImg=False)
+            
+            x_train,y_train = train
+            x_val,y_val = val
+
+            #Labels should be converted to categorical representation
+            y_train = to_categorical(y_train,self._ds.nclasses)
+            y_val = to_categorical(y_val,self._ds.nclasses)
+            train_generator = train_prep.flow(x_train,y_train,batch_size=self._config.batch_size,shuffle=True)
+            val_generator = val_prep.flow(x_val,y_val,batch_size=1)
+
+        return (train_generator,val_generator)
+    
+    def train_model(self,model):
+        """
+        Generic trainer. Receives a GenericModel and trains it
+        """
+        rcomp = re.compile(self._rex)
+        
+        # session setup
+        sess = K.get_session()
+        ses_config = tf.ConfigProto(
+            device_count={"CPU":self._config.cpu_count,"GPU":self._config.gpu_count},
+            intra_op_parallelism_threads=self._config.cpu_count if self._config.gpu_count == 0 else self._config.gpu_count, 
+            inter_op_parallelism_threads=self._config.cpu_count if self._config.gpu_count == 0 else self._config.gpu_count,
+            log_device_placement=True if self._verbose > 1 else False
+            )
+        sess.config = ses_config
+        K.set_session(sess)
+        
+        #Setup of generators, augmentation, preprocessing
+        train_data,val_data,_ = self._ds.split_metadata(self._config.split)                    
+        if self._verbose > 0:
+            unique,count = np.unique(train_data[1],return_counts=True)
+            l_count = dict(zip(unique,count))
+            print("Train labels: {0} are 0; {1} are 1;\n - {2:.2f} are positives".format(l_count[0],l_count[1],(l_count[1]/(l_count[0]+l_count[1]))))
+            
+            unique,count = np.unique(val_data[1],return_counts=True)
+            l_count = dict(zip(unique,count))
+            print("Validation labels: {0} are 0; {1} are 1;\n - {2:.2f} are positives".format(l_count[0],l_count[1],(l_count[1]/(l_count[0]+l_count[1]))))
+            
+            print("Train set: {0} items".format(len(train_data[0])))
+            print("Validate set: {0} items".format(len(val_data[0])))
+
+        train_generator,val_generator = self._choose_generator(train_data,val_data)
 
         single,parallel = model.build()
         if not parallel is None:
@@ -312,7 +219,7 @@ class Trainer(object):
             self._config.weights_path, wf_header + "e{epoch:02d}.h5"), 
             save_weights_only=True, period=1,save_best_only=True,monitor='val_acc'))
         ## ReduceLROnPlateau
-        callbacks.append(ReduceLROnPlateau(monitor='loss',factor=0.5,\
+        callbacks.append(ReduceLROnPlateau(monitor='loss',factor=0.7,\
                                            patience=5,verbose=self._verbose,\
                                            mode='auto',min_lr=1e-7))
         callbacks.append(LearningRateScheduler(_reduce_lr_on_epoch,verbose=1))
@@ -324,10 +231,10 @@ class Trainer(object):
 
         training_model.fit_generator(
             generator = train_generator,
-            steps_per_epoch = len(train_data[0]) // self._config.batch_size,
+            steps_per_epoch = len(train_generator), #// self._config.batch_size,
             epochs = self._config.epochs,
             validation_data = val_generator,
-            validation_steps = len(val_data[0]) //self._config.batch_size,
+            validation_steps = len(val_generator), #//self._config.batch_size,
             verbose = 1 if self._verbose > 0 else 0,
             use_multiprocessing = False,
             workers=self._config.cpu_count*2,
