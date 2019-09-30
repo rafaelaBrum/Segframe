ba5e3e295d2c5f830211761a724c2341a2a384a1
André LS Meirelles <alsmeirelles@gmail.com>
2019-09-30 19:30:58 -0300

    Adding debug to km_uncert

--- AL/BayesianFunctions.py
+++ AL/BayesianFunctions.py
@@ -141,7 +141,10 @@ def km_uncert(bayesian_model,generator,data_size,**kwargs):
         for k in range(len(un_clusters)):
             ind = np.asarray(un_clusters[k])
             print("Cluster {}, # of items: {}".format(k,ind.shape[0]))
-            print("Cluster {} first 30 item indexes: {}".format(k,un_indexes[ind]))
+            posa = np.ndarray(shape=(1,),dtype=np.int32)
+            for ii in range(30):
+                posa = np.hstack((posa,np.where(un_indexes == ind[ii])[0]))
+            print("Cluster {} first 30 items positions in index array: {}".format(k,posa))
             
     ac_count = 0
     acquired = []
