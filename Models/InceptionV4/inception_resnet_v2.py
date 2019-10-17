eb8792f967e50d7efe0b336b684bb30f7f541b62
André LS Meirelles <alsmeirelles@gmail.com>
2019-10-17 08:12:54 -0400

    Running Inception Model

--- Models/InceptionV4/inception_resnet_v2.py
+++ Models/InceptionV4/inception_resnet_v2.py
@@ -291,7 +291,7 @@ def InceptionResNetV2(include_top=True,
                                     block_type='block17',
                                     block_idx=block_idx,
                                     use_bn=use_bn)
-        x = layers.Dropout(0.1)(x,training=kwargs['training'])
+        x = layers.Dropout(0.2)(x,training=kwargs['training'])
 
     # Mixed 7a (Reduction-B block): 8 x 8 x 2080
     branch_0 = conv2d_bn(x, 256, 1, use_bn=use_bn)
@@ -312,7 +312,7 @@ def InceptionResNetV2(include_top=True,
                                     block_type='block8',
                                     block_idx=block_idx,
                                     use_bn=use_bn)
-        x = layers.Dropout(0.1)(x,training=kwargs['training'])
+        x = layers.Dropout(0.3)(x,training=kwargs['training'])
         
     x = inception_resnet_block(x,
                                 scale=1.,
