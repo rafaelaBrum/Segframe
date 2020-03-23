a0ac7767de55236c54e504cc1d37904c44c54ca9
André LS Meirelles <alsmeirelles@gmail.com>
2020-03-23 14:03:45 -0400

    Batch job adjustments

--- batch_jobs/wsi_tiler.sh
+++ batch_jobs/wsi_tiler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -p GPU-shared
-#SBATCH -t 30:00:00
+#SBATCH -t 10:00:00
 #SBATCH --gres=gpu:k80:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=alsmeirelles@gmail.com
@@ -8,7 +8,6 @@
 #echo commands to stdout
 #set -x
 
-DIRID="EN-5"
 export PYTHONPATH=$HOME/.local/lib/python3.6/site-packages:/pylon5/ac3uump/alsm/lib64/python3.6/site-packages:$PYTHONPATH
 
 cd /pylon5/ac3uump/alsm/active-learning/Segframe
@@ -22,7 +21,7 @@ module load openslide/3.4.1
 echo '[START] training'
 date +"%D %T"
 
-time python3 Utils/WSITile.py -ds ../data/wsis/ -od ../data/extracted -hm ../data/til_maps/TIL_maps_after_thres_v1/ -mp 3 -ps 100 -wr 0.25 -txt_label -hmc
+time python3 Utils/WSITile.py -ds ../data/wsis-ext/ -od ../data/nds300 -hm ../data/til_maps/TIL_maps_after_thres_v1/ -mp 4 -ps 300 -wr 0.25 -txt_label -hmc
 
 echo '[FINAL] done training'
 
