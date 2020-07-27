4c86a2e1f3d8ca8c380c8ee22c77597dabdae52f
André LS Meirelles <alsmeirelles@gmail.com>
2020-07-27 18:23:44 -0400

    Improvements to SetRecluster

--- batch_jobs/inception.sh
+++ batch_jobs/inception.sh
@@ -33,7 +33,7 @@ module load cuda/9.0
 echo '[START] training'
 date +"%D %T"
 
-time python3 main.py -i -v --train --pred -predst /pylon5/ac3uump/alsm/active-learning/data/nds300 -split 0.92 0.01 0.07 -net Inception -data CellRep -d -e 50 -b 62 -tdim 240 240 -f1 10 -out logs/ -cpu 9 -gpu 2 -tn -sv -nsw -wpath results/$DIRID -model_dir results/$DIRID -logdir results/$DIRID -cache results/$DIRID -sample 100000 -wsi_split 5 -pred_size 15000 
+time python3 main.py -i -v --train --pred -predst /pylon5/ac3uump/alsm/active-learning/data/nds300 -split 0.9 0.01 0.09 -net Inception -data CellRep -d -e 50 -b 62 -tdim 240 240 -f1 10 -out logs/ -cpu 9 -gpu 2 -tn -sv -nsw -wpath results/$DIRID -model_dir results/$DIRID -logdir results/$DIRID -cache results/$DIRID -sample 100000 -wsi_split 5 -pred_size 15000 
 
 echo '[FINAL] done training'
 
