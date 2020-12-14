295d303a2193f27c8f1c9b576fd6ee76130e2105
André LS Meirelles <alsmeirelles@gmail.com>
2020-12-13 22:09:17 -0300

    Solving bug. Samples were being mixed.

--- Trainers/BatchGenerator.py
+++ Trainers/BatchGenerator.py
@@ -313,21 +313,21 @@ class ThreadedGenerator(GenericIterator):
         # generate a random batch of points
         X = self.data[0]
         Y = self.data[1]
-        futures = []
+        futures = {}
 
         for i,j in enumerate(index_array):
-            futures.append(self._executor.submit(self._thread_run_images,X[j],Y[j],self.keep))
+            futures[self._executor.submit(self._thread_run_images,X[j],Y[j],self.keep)] = i
 
         i=0
         for f in concurrent.futures.as_completed(futures):
             # add point to x_batch and diagnoses to y
             example,t_y = f.result()
+            i = futures[f]
             if batch_x is None:
                 self.shape = example.shape
                 batch_x = np.zeros(tuple([len(index_array)] + list(self.shape)),dtype=np.float32)            
             batch_x[i] = example
             y[i] = t_y
-            i += 1
 
         #Always normalize
         batch_x = self.image_generator.standardize(batch_x)
